9e6c91c8e1914808353c8ff6bb1ae2196fc03a55
JamalArsh <92667028+JamalArsh@users.noreply.github.com>
2024-04-25 22:08:17 +0530

    siwpper base added

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
 import "./style.scss";
+// import Swiper bundle with all modules installed
+import Swiper from "swiper/bundle";
+
+// import styles bundle
+import "swiper/css/bundle";
+
+var swiper = new Swiper(".mySwiper-1", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// init Swiper:
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
